7e8e8fa08dec94bf5b9ecf951a3aa098c394c208
OkadaGenya <g.okada.wilf+github@gmail.com>
2018-12-21 11:27:18 +0900

    add nodemon & watch agreed dir

--- agreed/api/user/get/index.js
+++ agreed/api/user/get/index.js
@@ -1,26 +1,23 @@
 module.exports = {
-    request: {
-        path: '/user/:id',
-        method: 'GET',
+  request: {
+    path: '/user/:id',
+    method: 'GET',
+  },
+  response: {
+    headers: {
+      'x-csrf-token': 'csrf-token',
     },
-    response: {
-        headers: {
-            'x-csrf-token': 'csrf-token',
-        },
-        body: {
-            message: '{:greeting} {:id} {:someQueryStrings}',
-            images: '{:images}',
-            themes: '{:themes}',
-        },
-        values: {
-            greeting: 'hello',
-            images: [
-                'http://example.com/foo.jpg',
-                'http://example.com/bar.jpg',
-            ],
-            themes: {
-                name: 'green',
-            },
-        },
+    body: {
+      message: '{:greeting} {:id} {:someQueryStrings}',
+      images: '{:images}',
+      themes: '{:themes}',
     },
+    values: {
+      greeting: 'hello',
+      images: ['http://example.com/foo.jpg', 'http://example.com/bar.jpg'],
+      themes: {
+        name: 'green',
+      },
+    },
+  },
 }
